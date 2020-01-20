69832a22e1f6aee5dbf90cc073ff761e4bfe21c7
Ciervos <mlujafo@gmail.com>
2020-01-20 12:42:52 -0300

    quitado subrayado de link

--- src/components/Ejerciciowonder/Ejerciciowonder.js
+++ src/components/Ejerciciowonder/Ejerciciowonder.js
@@ -10,7 +10,7 @@ return(
  <section  id="contenedorblanco" className="tarjeta-contenedor-blanco">
     <header><h2>Mitologia griega</h2><h1>Medusa</h1></header>
     <article>
-    <span class="tarjeta-contenedor-blanco-txt">En la mitología griega, Medusa era un monstruo ctónico femenino, que convertía en piedra a aquellos que la miraban fijamente a los ojos. Fue decapitada por Perseo, quien después usó su cabeza como arma2 hasta que se la dio a la diosa Atenea para que le pusiera en su escudo, la égida. Desde la antigüedad clásica griega, la imagen de la cabeza de Medusa aparece representada en el artilugio que aleja el mal conocido como Gorgoneion.</span>
+    <span class="tarjeta-contenedor-blanco-txt">En la mitología griega, Medusa era un monstruo ctónico femenino, que convertía en piedra a aquellos que la miraban fijamente a los ojos. Fue decapitada por Perseo, quien después usó su cabeza como arma hasta que se la dio a la diosa Atenea para que le pusiera en su escudo, la égida. Desde la antigüedad clásica griega, la imagen de la cabeza de Medusa aparece representada en el artilugio que aleja el mal conocido como Gorgoneion.</span>
 
     <p><a className="tarjetalink" href="https://reactjs.org" target="_blank"  rel="noopener noreferrer">
 👀 No mires a sus ojos
